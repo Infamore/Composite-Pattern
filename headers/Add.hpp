a25f0ad4636b52a8453e907aba18c2953aca8a66
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:54:16 -0800

    Update Add.hpp

--- headers/Add.hpp
+++ headers/Add.hpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-class add : public op, private Base {				
+class Add : public op, private Base {				
         public:
 
                 add(Base l1, Base r1) : Base(l1, r1) { left = l1; right = r1; }

a20af407b81f707094e51d6b3a35be8aa9e0120b
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:49:52 -0800

    Rand.hpp

--- headers/Rand.hpp
+++ headers/Rand.hpp
@@ -2,27 +2,17 @@
 #include <ctime>
 #include <string>
 #include <sstream>
-/*
-string tostring(const double a_value, const int n = 0)
-{
-        std::ostringstream out;
-        out.precision(n);
-        out << std::fixed << a_value;
-        return out.str();
-}
-*/
 
-class Rand : public Base {
+using namespace std;
+
+class Rand : public op, private Base {
         public:
 
-        Rand() {operand = rand() % 100;}
+        Rand() { return operand = 100 + rand() % 100;}
 
-        double evaluate() {return operand;}
+        double evaluate() { return operand; }
 
-        std::string stringify() {
-        std::string value = std::to_string(operand);
-        return value;
-        }
+        std::string stringify() { return std::string value = operand; }
 
         private:
         double operand;

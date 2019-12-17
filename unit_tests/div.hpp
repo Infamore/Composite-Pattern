6ca9cad4b3870f40210d385690277ebde3ff5dba
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:04:04 -0800

    Create div.hpp

---
+++ unit_tests/div.hpp
@@ -0,0 +1,18 @@
+#include "div.hpp"
+#include "base.hpp"
+#include "gtest/gtest.h"
+
+TEST(classtesting, divclass) {
+        op* op1 = new op(15);
+        op* op2 = new op(3);
+        Div* temp = new Div(op1, op2);
+        EXPECT_EQ(5, temp->evaluate());
+        EXPECT_EQ("15 / 3"), temp->stringify());
+
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

d0ad2f6621bde85e17f6f3c791fe244ca5fcfe8b
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:45:54 -0800

    Add.hpp

--- headers/Add.hpp
+++ headers/Add.hpp
@@ -4,26 +4,16 @@
 
 using namespace std;
 
-class add : public Base {				
+class add : public op, private Base {				
         public:
 
-                add(Base l1, Base r1) : Base(l1, r1) {
-                        left = l1;
-                        right = r1;
-                }
+                add(Base l1, Base r1) : Base(l1, r1) { left = l1; right = r1; }
                 
-                double evaluate() {
-                 return double sum = left->evaluate() + right->evaluate();
-                }
+                double evaluate() { return double sum = left->evaluate() + right->evaluate(); }
 
-                std::string stringify() {
-                return std::string value = stringify() + " + " + stringify();
-                 
-
-                }
+                std::string stringify() { return std::string value = stringify() + " + " + stringify(); }
                 
-					private:
-								Base* left;
-								Base* right;
-								
+	private:
+		Base* left;
+		Base* right;						
 };

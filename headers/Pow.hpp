604a07033179757501c0439e0c9ae6543a5bf58b
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:52:12 -0800

    Pow.hpp

--- headers/Pow.hpp
+++ headers/Pow.hpp
@@ -2,29 +2,21 @@
 #include "op.hpp"
 #include <string>
 
-class Pow : public op
+class Pow : public op, private Base
 {
 	private:
   Base* left;
   Base* right;
   
   public:
-  Pow(Base* l1, Base* r1) {
-  left = l1;
-  right = r1;
-  }
+  Pow(Base l1, Base r1) : Base(l1, r1) { left = l1; right = r1; }
   
   double evaluate() {
-  		double product = 1;
+  	double product = 1;
       for (int i = 0; i < right->evaluate(); i++)
-      	product *= left->evaluate();
+      	double product *= left->evaluate();
       return product;
   }
 
-	string stringify() {
-
-  	string value = left->stringify() + " ** " + right ->stringify();
-
-  	return value;
-	}
+	std::string stringify() { std::string value = left->stringify() + " ** " + right ->stringify(); }
 };

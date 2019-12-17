9ff74df0d6c0fa483c7b141c362aa6f87461bf0d
esoud <35392150+esoud@users.noreply.github.com>
2019-12-16 17:43:38 -0800

    Div.hpp

--- headers/Div.hpp
+++ headers/Div.hpp
@@ -7,17 +7,14 @@ using namespace std;
 class Div : public op, private Base
 {
 
-	private:
-  Base* left;
-  Base* right;
-  
+  private:
+  	Base* left;
+  	Base* right;
+	
   public:
-	Div(Base l1, Base r1) : Base(l1, r1) {
-	left = l1;
-        right = r1;
-  }
+	Div(Base l1, Base r1) : Base(l1, r1) { left = l1; right = r1; }
   
-  double evaluate() { return double quotient = left->evaluate() / right->evaluate(); }
+  	double evaluate() { return double quotient = left->evaluate() / right->evaluate(); }
   
-  std::string stringify() { return std::string value = left->stringify() + " / " + right->stringify(); }
+  	std::string stringify() { return std::string value = left->stringify() + " / " + right->stringify(); }
 };
